0b8390d1b46e44f5e4fc71f41341e060af5155a1
RingoUaOd <89596527+RingoUaOd@users.noreply.github.com>
2021-08-27 13:44:32 +0300

    Add files via upload

---
+++ Lesson_02_Escape_sequences.py
@@ -0,0 +1,26 @@
+# № 1
+
+print('Escape sequences')
+print('\\a      Bell (alert)')
+print('\\b      Backspace')
+print('\\n      New line')
+print('\\t      Horizontal tab')
+print('\\\\      Backslash \\')
+print('\\"      Double quotation mark "')
+print("\\'      Single quotation mark '")
+
+# № 2
+
+print('Escape sequences')
+print('\\a\t\tBell (alert)')
+print('\\b\t\tBackspace')
+print('\\n\t\tNew line')
+print('\\t\t\tHorizontal tab')
+print('\\\\\t\tBackslash \\')
+print('\\\"\t\tDouble quotation mark \"')
+print('\\\'\t\tSingle quotation mark \'')
+
+# № 3
+
+print('Escape sequences\n\\a\t\tBell (alert)\n\\b\t\tBackspace\n\\n\t\tNew line\n\\t\t\tHorizontal tab\n'
+      '\\\\\t\tBackslash \\\n\\\"\t\tDouble quotation mark \"\n\\\'\t\tSingle quotation mark \'')
